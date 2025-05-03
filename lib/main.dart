e9303d04f80e2e02ec49230ff78792b13d90c099
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2025-05-04 01:01:57 +0600

    feat: added dnd schedule via AlarmManager

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 
-void main() {
+void main() async {
+  // This needs to be called before any platform channel calls
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize AndroidAlarmManager
+  await AndroidAlarmManager.initialize();
+
   runApp(MyApp());
 }
 
