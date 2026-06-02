c94cab287ac327249c4970a6f142ab570a49e1bf
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-06-02 19:13:24 +0600

    discrete function for sending token to backend

--- lib/pages/seat_status.dart
+++ lib/pages/seat_status.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:preconnect/api/seat_status.dart';
