3a0940ef77e66b93df5338791225946b85172841
sabbirba <sabbirba10@gmail.com>
2026-06-23 01:04:43 +0600

    perf: optimize headers and body forwarding in proxy worker

--- index.js
+++ index.js
@@ -12,18 +12,28 @@ export default {
           "Access-Control-Allow-Headers": "Authorization, Content-Type, X-REALM, X-SOURCE, X-ID-Token, Accept, Accept-Language, If-None-Match, If-Match",
           "Access-Control-Allow-Credentials": "true",
           "Access-Control-Max-Age": "86400",
-        }
+        },
       });
     }
 
     let targetUrl = null;
+    let targetHost = "";
+    let targetOrigin = "";
 
     if (path.startsWith("/api/")) {
       targetUrl = "https://connect.bracu.ac.bd" + path;
+      targetHost = "connect.bracu.ac.bd";
+      targetOrigin = "https://connect.bracu.ac.bd";
     } else if (path.startsWith("/cdn/")) {
       targetUrl = "https://connect.bracu.ac.bd" + path;
+      targetHost = "connect.bracu.ac.bd";
+      targetOrigin = "https://connect.bracu.ac.bd";
     } else if (path.startsWith("/sso/")) {
-      targetUrl = "https://sso.bracu.ac.bd/realms/bracu/protocol/openid-connect/" + path.substring(5);
+      targetUrl =
+        "https://sso.bracu.ac.bd/realms/bracu/protocol/openid-connect/" +
+        path.substring(5);
+      targetHost = "sso.bracu.ac.bd";
+      targetOrigin = "https://sso.bracu.ac.bd";
     }
 
     if (!targetUrl) {
@@ -35,27 +45,34 @@ export default {
     }
 
     const headers = new Headers(request.headers);
-    headers.set("Host", new URL(targetUrl).host);
-    headers.set("Origin", new URL(targetUrl).origin);
-    headers.set("Referer", new URL(targetUrl).origin + "/");
+    headers.set("Host", targetHost);
+    headers.set("Origin", targetOrigin);
+    headers.set("Referer", targetOrigin + "/");
 
+    const hasBody = request.method !== "GET" && request.method !== "HEAD";
     try {
       const response = await fetch(targetUrl, {
         method: request.method,
         headers: headers,
-        body: request.body,
-        redirect: "manual"
+        body: hasBody ? request.body : undefined,
+        redirect: "manual",
       });
 
       const responseHeaders = new Headers(response.headers);
-      responseHeaders.set("Access-Control-Allow-Origin", request.headers.get("Origin") || "*");
+      responseHeaders.set(
+        "Access-Control-Allow-Origin",
+        request.headers.get("Origin") || "*",
+      );
       responseHeaders.set("Access-Control-Allow-Credentials", "true");
-      responseHeaders.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+      responseHeaders.set(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE, PATCH",
+      );
 
       return new Response(response.body, {
         status: response.status,
         statusText: response.statusText,
-        headers: responseHeaders
+        headers: responseHeaders,
       });
     } catch (err) {
       return new Response(JSON.stringify({ error: err.message }), {
@@ -63,8 +80,8 @@ export default {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
-        }
+        },
       });
     }
-  }
+  },
 };
