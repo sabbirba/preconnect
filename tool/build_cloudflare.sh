ef2bc8dab544256fc002aca1cfe3214165be84c8
sabbirba <sabbirba10@gmail.com>
2026-06-27 22:10:55 +0600

    Revert "chore: remove unused package.json and build_cloudflare.sh"
    This reverts commit 54976ad7dc8ae2ef0fbb1b8bf6c9c76e0e73fb7d.

---
+++ tool/build_cloudflare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+FLUTTER_DIR="$HOME/flutter"
+if [ ! -d "$FLUTTER_DIR" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$FLUTTER_DIR"
+fi
+export PATH="$FLUTTER_DIR/bin:$PATH"
+flutter doctor
+if [ ! -f ".env" ]; then
+  touch .env
+  for key in storeFile storePassword keyAlias keyPassword DEVELOPMENT_TEAM GITHUB_TOKEN; do
+    val=$(eval echo \$$key)
+    if [ -n "$val" ]; then
+      echo "$key=$val" >> .env
+    fi
+  done
+  env | grep -E '^(APP_|PRECONNECT_)' >> .env || true
+fi
+flutter pub get
+flutter build web --release --tree-shake-icons --csp --no-wasm-dry-run --no-web-resources-cdn --dart-define-from-file=.env
