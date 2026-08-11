ae81383f299a818697acb439f1f8b638cdd7562f
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-08-11 10:55:59 +0600

    remove preprintd from source to add as submodule

--- preprintd/src/utils.rs
+++
@@ -1,8 +0,0 @@
-use uuid::Uuid;
-
-pub fn create_new_ident() -> String {
-    let mut ident = Uuid::new_v4().to_string();
-    ident.push('_');
-    ident.push_str(std::env::consts::ARCH);
-    ident
-}
